3bcc583d08d3a5701ccb319a519fcd50c218386c
PJ <pj@notanemail.com>
2021-10-21 17:06:25 -0400

    Moved most of the CLI code out of the main module

--- src/stringventory.cr
+++ src/stringventory.cr
@@ -58,167 +58,170 @@ module Stringventory
       puts "No #{res} found!"
     end
   end
-
-  # For this default, this needs to have 3 /'s on the sqlite protocol portion,
-  # as the leading / from ENV["HOME"] will get dropped.
-  db_url = File.join("sqlite3:///", ENV["HOME"], ".local", "share", "stringventory.db")
-  sub_c = StrVResource::None
-  comm = StrVAction::None
-
-  options = {
-    :num_strs => 6,
-    :name => "",
-    :str_name => "",
-    :num_packs => 1,
-  }
-
-  # Save the help message if it's needed later.
-  help_message = ""
-
-  parser = OptionParser.new do |parser|
-
-    parser.banner = "Usage: stringventory [options] [command] [arguments]"
-
-    parser.on("-d PATH", "--database=PATH", "Specify the database path (default = #{db_url})") { |pth| db_url = pth }
-
-    parser.on("guitars", "Manage guitars") do
-      sub_c = StrVResource::Guitar
-      parser.banner = "Usage: stringventory guitars [options] [command] [arguments]"
-
-      parser.on("add", "Add a new guitar") do
-        comm = StrVAction::Create
-        common_opts(parser, sub_c, options)
-        help_message = parser.to_s
-      end
-
-      # No update method, since it doesn't make _that_ much sense.
-
-      parser.on("restring", "Restring a guitar") do
-        comm = StrVAction::StringChange
-        common_opts(parser, sub_c, options, strings: false)
-        parser.on("-r STRS", "--restring-with=STRS", "Strings to restring the guitar with") { |strs| options[:str_name] = strs }
-        help_message = parser.to_s
-      end
-      parser.on("remove", "Remove a guitar") do
-        comm = StrVAction::Delete
-        common_opts(parser, sub_c, options, strings: false)
-        help_message = parser.to_s
-      end
-      parser.on("list", "List guitars") do
-        comm = StrVAction::List
-        common_opts(parser, sub_c, options, strings: false)
-
-        # This one should never fail due to missing name
-      end
-
-    end
-
-    parser.on("strings", "Manage strings") do
-      sub_c = StrVResource::Strings
-      parser.banner = "Usage: stringventory strings [options] [command] [arguments]"
-
-      help_message = parser.to_s
-
-      parser.on("add", "Add a new string set") do
-        comm = StrVAction::Create
-
-        common_opts(parser, sub_c, options)
-
-        help_message = parser.to_s
-      end
-      parser.on("remove", "Remove a string set") do
-        comm = StrVAction::Delete
-
-        common_opts(parser, sub_c, options, strings: false, packs: false)
-
-        help_message = parser.to_s
-      end
-      parser.on("bought", "Add strings to the current stock") do
-        comm = StrVAction::Update
-        common_opts(parser, sub_c, options, strings: false)
-        help_message = parser.to_s
-      end
-
-      parser.on("used", "Remove strings from the current stock outside of a string change") do
-        comm = StrVAction::StringChange
-        common_opts(parser, sub_c, options, strings: false)
-        help_message = parser.to_s
-      end
-
-      parser.on("list", "List string packs") do
-        comm = StrVAction::List
-        common_opts(parser, sub_c, options, strings: false, packs: false)
-        help_message = parser.to_s
-      end
-
-    end
-
-    parser.on("database", "Manage the database") do
-      sub_c = StrVResource::Database
-      parser.banner = "Usage: stringventory database [options] [command] [arguments]"
-
-      help_message = parser.to_s
-
-      parser.on("create", "Create the database") { comm = StrVAction::Create }
-      parser.on("update", "Drop and recreate the database") { comm = StrVAction::Update }
-      parser.on("drop", "Drop the database and all data") { comm = StrVAction::Delete }
-
-    end
-
-    parser.on("-h", "--help") do
-      puts parser
-      exit
-    end
-  end
-
-  parser.parse
-
-  Granite::Connections << Granite::Adapter::Sqlite.new(name: "con", url: db_url)
-
-  case sub_c
-  when StrVResource::Guitar
-
-    validate_name(options, help_message, comm)
-
-    gtrs = Actions::Guitars.process_action(act: comm,
-                                           name: options[:name].to_s,
-                                           num_strs: options[:num_strs].to_i32,
-                                           str_name: options[:str_name].to_s)
-
-    print_output act: comm, res: "guitars", outp: gtrs
-
-  when StrVResource::Strings
-
-    validate_name(options, help_message, comm)
-
-    packs = Actions::Strings.process_action(act: comm,
-                                            name: options[:name].to_s,
-                                            num_strs: options[:num_strs].to_i32,
-                                            num_packs: options[:num_packs].to_i32)
-
-    print_output act: comm, res: "string packs", outp: packs
-
-  when StrVResource::Database
-    res = Actions::Database.process_action(comm)
-
-    case res
-    when String
-      puts res
-    when Exception
-      STDERR.puts res.message
-      exit 2
-    when Nil
-      STDERR.puts "Action not recognized!"
-      exit 5
-    end
-
-  else
-    STDERR.puts parser
-    exit 1
-  end
-
 end
+
+# For this default, this needs to have 3 /'s on the sqlite protocol portion,
+# as the leading / from ENV["HOME"] will get dropped.
+db_url = File.join("sqlite3:///", ENV["HOME"], ".local", "share", "stringventory.db")
+sub_c = Stringventory::StrVResource::None
+comm = Stringventory::StrVAction::None
+
+options = {
+  :num_strs => 6,
+  :name => "",
+  :str_name => "",
+  :num_packs => 1,
+}
+
+# Save the help message if it's needed later.
+help_message = ""
+
+parser = OptionParser.new do |parser|
+
+  parser.banner = "Usage: stringventory [options] [command] [arguments]"
+
+  parser.on("-d PATH", "--database=PATH", "Specify the database path (default = #{db_url})") { |pth| db_url = pth }
+
+  parser.on("guitars", "Manage guitars") do
+    sub_c = Stringventory::StrVResource::Guitar
+    parser.banner = "Usage: stringventory guitars [options] [command] [arguments]"
+
+    parser.on("add", "Add a new guitar") do
+      comm = Stringventory::StrVAction::Create
+      Stringventory.common_opts(parser, sub_c, options)
+      help_message = parser.to_s
+    end
+
+    # No update method, since it doesn't make _that_ much sense.
+
+    parser.on("restring", "Restring a guitar") do
+      comm = Stringventory::StrVAction::StringChange
+      Stringventory.common_opts(parser, sub_c, options, strings: false)
+      parser.on("-r STRS", "--restring-with=STRS", "Strings to restring the guitar with") { |strs| options[:str_name] = strs }
+      help_message = parser.to_s
+    end
+    parser.on("remove", "Remove a guitar") do
+      comm = Stringventory::StrVAction::Delete
+      Stringventory.common_opts(parser, sub_c, options, strings: false)
+      help_message = parser.to_s
+    end
+    parser.on("list", "List guitars") do
+      comm = Stringventory::StrVAction::List
+      Stringventory.common_opts(parser, sub_c, options, strings: false)
+
+      # This one should never fail due to missing name
+    end
+
+  end
+
+  parser.on("strings", "Manage strings") do
+    sub_c = Stringventory::StrVResource::Strings
+    parser.banner = "Usage: stringventory strings [options] [command] [arguments]"
+
+    help_message = parser.to_s
+
+    parser.on("add", "Add a new string set") do
+      comm = Stringventory::StrVAction::Create
+
+      Stringventory.common_opts(parser, sub_c, options)
+
+      help_message = parser.to_s
+    end
+    parser.on("remove", "Remove a string set") do
+      comm = Stringventory::StrVAction::Delete
+
+      Stringventory.common_opts(parser, sub_c, options, strings: false, packs: false)
+
+      help_message = parser.to_s
+    end
+    parser.on("bought", "Add strings to the current stock") do
+      comm = Stringventory::StrVAction::Update
+      Stringventory.common_opts(parser, sub_c, options, strings: false)
+      help_message = parser.to_s
+    end
+
+    parser.on("used", "Remove strings from the current stock outside of a string change") do
+      comm = Stringventory::StrVAction::StringChange
+      Stringventory.common_opts(parser, sub_c, options, strings: false)
+      help_message = parser.to_s
+    end
+
+    parser.on("list", "List string packs") do
+      comm = Stringventory::StrVAction::List
+      Stringventory.common_opts(parser, sub_c, options, strings: false, packs: false)
+      help_message = parser.to_s
+    end
+
+  end
+
+  parser.on("database", "Manage the database") do
+    sub_c = Stringventory::StrVResource::Database
+    parser.banner = "Usage: stringventory database [options] [command] [arguments]"
+
+    help_message = parser.to_s
+
+    parser.on("create", "Create the database") { comm = Stringventory::StrVAction::Create }
+    parser.on("update", "Drop and recreate the database") { comm = Stringventory::StrVAction::Update }
+    parser.on("drop", "Drop the database and all data") { comm = Stringventory::StrVAction::Delete }
+
+  end
+
+  parser.on("-h", "--help", "Print this help message") do
+    puts parser
+    exit
+  end
+end
+
+parser.parse
+
+# Really needed this stuff out of the main module, since granite really needs
+# to be required before anything happens with the models, otherwise validations
+# don't work
+Granite::Connections << Granite::Adapter::Sqlite.new(name: "con", url: db_url)
 
 require "granite"
 require "granite/adapter/sqlite"
 require "./actions/*"
+
+case sub_c
+when Stringventory::StrVResource::Guitar
+
+  Stringventory.validate_name(options, help_message, comm)
+
+  gtrs = Stringventory::Actions::Guitars.process_action(act: comm,
+                                          name: options[:name].to_s,
+                                          num_strs: options[:num_strs].to_i32,
+                                          str_name: options[:str_name].to_s)
+
+  Stringventory.print_output act: comm, res: "guitars", outp: gtrs
+
+when Stringventory::StrVResource::Strings
+
+  Stringventory.validate_name(options, help_message, comm)
+
+  packs = Stringventory::Actions::Strings.process_action(act: comm,
+                                          name: options[:name].to_s,
+                                          num_strs: options[:num_strs].to_i32,
+                                          num_packs: options[:num_packs].to_i32)
+
+  Stringventory.print_output act: comm, res: "string packs", outp: packs
+
+when Stringventory::StrVResource::Database
+  res = Stringventory::Actions::Database.process_action(comm)
+
+  case res
+  when String
+    puts res
+  when Exception
+    STDERR.puts res.message
+    exit 2
+  when Nil
+    STDERR.puts "Action not recognized!"
+    exit 5
+  end
+
+else
+  STDERR.puts parser
+  exit 1
+end
+
