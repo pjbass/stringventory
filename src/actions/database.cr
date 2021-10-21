ea0236f9d4b85f84aefe97c3ec6b661f179798b4
PJ <pj@notanemail.com>
2021-10-21 14:09:33 -0400

    Shifted the begin-rescue block outside of the case statement

--- src/actions/database.cr
+++ src/actions/database.cr
@@ -7,31 +7,28 @@ module Stringventory::Actions::Database
   # Method to process actions
   def self.process_action(act : StrVAction) : Exception|String|Nil
 
-    case act
-    when StrVAction::Create
-      begin
-        Models::Guitar.migrator.create
-        Models::Strings.migrator.create
-        "Database successfully created!"
-      rescue e
-        e
+    begin
+      case act
+      when StrVAction::Create
+          Models::Guitar.migrator.create
+          Models::Strings.migrator.create
+          Models::StringChange.migrator.create
+          "Database successfully created!"
+      when StrVAction::Update
+        begin
+          Models::Guitar.migrator.drop_and_create
+          Models::Strings.migrator.drop_and_create
+          Models::StringChange.migrator.drop_and_create
+          "Database dropped and recreated!"
+      when StrVAction::Delete
+        begin
+          Models::Guitar.migrator.drop
+          Models::Strings.migrator.drop
+          Models::StringChange.migrator.drop
+          "Database dropped!"
       end
-    when StrVAction::Update
-      begin
-        Models::Guitar.migrator.drop_and_create
-        Models::Strings.migrator.drop_and_create
-        "Database dropped and recreated!"
-      rescue e
-        e
-      end
-    when StrVAction::Delete
-      begin
-        Models::Guitar.migrator.drop
-        Models::Strings.migrator.drop
-        "Database dropped!"
-      rescue e
-        e
-      end
+    rescue e
+      e
     end
 
   end
